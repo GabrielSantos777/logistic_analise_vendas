e8541eb0f7b71639e57a7b55a163bcd38d95d567
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-04-24 20:34:46 -0300

    feat(frontend): Corrigir rota do backend para comunicar com o frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [msg, setMsg] = useState("");
 
   useEffect(() => {
-    api.get("/api")
+    api.get("/")
       .then(res => setMsg(res.data.message))
       .catch(() => setMsg("Erro ao conectar com backend"));
   }, []);
